b1fbedd21d6bd2b30683d6f74646d35ba9198a67
Ziad Mohamed Ismail <ziadmhmd991@gmail.com>
2023-09-19 10:57:07 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/posts" });
+const API = axios.create({ baseURL: "https://socialmedia-m94x.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
